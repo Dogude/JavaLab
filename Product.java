6f196cd7f0a3464b3810d800ccdcf38b50adba78
North <131747396+Dogude@users.noreply.github.com>
2026-05-08 15:59:23 +0200

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,29 @@
+package com.example.firstrestapi.product.domain;
+
+public class Product {
+
+    private long id;
+
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
