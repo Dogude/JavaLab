c436563e7146f7ff4df09412bc3fc249fdda817a
North <131747396+Dogude@users.noreply.github.com>
2026-05-08 16:02:12 +0200

    Create ProductNotFoundException.java

---
+++ ProductNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.firstrestapi.product.support.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+
+        super(String.format("Product with %d not found", id));
+
+    }
+
+}
