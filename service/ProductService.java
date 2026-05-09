1964bdd8c33584ba6c6e3275f1ee2c7ba21ad860
North <131747396+Dogude@users.noreply.github.com>
2026-05-09 17:11:18 +0200

    Update ProductService.java

--- service/ProductService.java
+++ service/ProductService.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ProductService {
+public class ProductService  {
 
 
     private final ProductRepository productRepository;
