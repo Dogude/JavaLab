5bef81820ac5c7f6ef60bd039c6228496a0f374c
North <131747396+Dogude@users.noreply.github.com>
2026-05-09 18:25:37 +0200

    Update OldProductRepository.java

--- repository/OldProductRepository.java
+++ repository/OldProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 @Repository
-public class ProductRepository {
+public class OldProductRepository {
 
     protected final Map<Long, Product> map = new HashMap<>();
 
